8f4aead04903b383e60df690ce4104b2f8811af6
Andrew Perry <ajperry@pansapiens.com>
2016-02-09 15:30:33 +1100

    Properly fix test using assertDictEqual instead.

--- tests/test_models.py
+++ tests/test_models.py
@@ -73,8 +73,8 @@ class IlluminaParserTestCase(unittest.TestCase):
 
     def test__to_parameter_schema(self):
         param_schema_dict = self.parameterset_model.to_parameter_schema()
-        self.assertEqual(param_schema_dict,
-                         [{u'pk': None,
+        self.assertDictEqual(param_schema_dict[0],
+                          {u'pk': None,
                            u'model': u'tardis_portal.parametername',
                            u'fields': {u'full_name': u'A float field',
                                        u'comparison_type': 1,
@@ -85,7 +85,8 @@ class IlluminaParserTestCase(unittest.TestCase):
                                        u'units': u'', u'order': 9999,
                                        u'immutable': True,
                                        u'is_searchable': True,
-                                       u'choices': u''}},
+                                       u'choices': u''}})
+        self.assertDictEqual(param_schema_dict[1],
                           {u'pk': None,
                            u'model': u'tardis_portal.parametername',
                            u'fields': {u'full_name': u'A string field',
@@ -97,7 +98,7 @@ class IlluminaParserTestCase(unittest.TestCase):
                                        u'units': u'', u'order': 9999,
                                        u'immutable': True,
                                        u'is_searchable': True,
-                                       u'choices': u''}}])
+                                       u'choices': u''}})
 
 
 if __name__ == '__main__':
