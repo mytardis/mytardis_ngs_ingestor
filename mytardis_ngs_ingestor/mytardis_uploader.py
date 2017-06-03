0d8edf4ec60eff68ec027f25a3deafe56da0fb81
Andrew Perry <ajperry@pansapiens.com>
2017-06-03 12:58:23 +1000

    Properly merge config dicts for run-specific overrides.

--- mytardis_ngs_ingestor/mytardis_uploader.py
+++ mytardis_ngs_ingestor/mytardis_uploader.py
@@ -49,19 +49,6 @@ except ImportError:
 DEFAULT_STORAGE_MODE = 'upload'
 
 
-# http://stackoverflow.com/a/26853961
-# Deprecated by use of toolz.dicttoolz.merge_dicts
-# def merge_dicts(*dict_args):
-#     """
-#     Given any number of dicts, shallow copy and merge into a new dict,
-#     precedence goes to key value pairs in latter dicts.
-#     """
-#     result = {}
-#     for dictionary in dict_args:
-#         result.update(dictionary)
-#     return result
-
-
 class TastyPieAuth(AuthBase):
     """
     Attaches HTTP headers for Tastypie API key Authentication to the given
